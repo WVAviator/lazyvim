ff619a710dbcb88c1eaa4f2ea305ca14d261e5ee
WVAviator <wvaviator@gmail.com>
2024-05-05 16:34:04 -0500

    configure nvim-jdtls with lombok

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,11 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Java
+vim.keymap.set("n", "<leader>Jo", "<cmd>lua require'jdtls'.organize_imports()<CR>", { desc = "Organize Imports" })
+vim.keymap.set("n", "<leader>Jv", "<cmd>lua require('jdtls').extract_variable()<CR>", { desc = "Extract Variable" })
+vim.keymap.set("n", "<leader>Jc", "<cmd>lua require('jdtls').extract_constant()<CR>", { desc = "Extract Constant" })
+vim.keymap.set("n", "<leader>Jt", "<cmd>lua require'jdtls'.test_nearest_method()<CR>", { desc = "Test Method" })
+vim.keymap.set("n", "<leader>JT", "<cmd>lua require'jdtls'.test_class()<CR>", { desc = "Test Class" })
+vim.keymap.set("n", "<leader>Ju", "<cmd>JdtUpdateConfig<CR>", { desc = "Update Config" })
 
--- Buffers
--- vim.keymap.del("n", "<leader>br")
--- vim.keymap.set("n", "<leader>bb", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
+vim.keymap.set("v", "<leader>Jv", "<Esc><cmd>lua require('jdtls').extract_variable(true)<CR>", { desc = "Extract Variable" })
+vim.keymap.set("v", "<leader>Jc", "<Esc><cmd>lua require('jdtls').extract_constant(true)<CR>", { desc = "Extract Constant" })
+vim.keymap.set("v", "<leader>Jm", "<Esc><cmd>lua require('jdtls').extract_method(true)<CR>", { desc = "Extract Method" })
